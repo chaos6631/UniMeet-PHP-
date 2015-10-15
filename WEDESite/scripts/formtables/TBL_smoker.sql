7af1c8f550f26d99bf3e87274fcce4bc2cc38ab6
chaos6631 <chriscalder@rogers.com>
2015-10-15 16:06:59 -0400

    delete folder

--- WEDESite/scripts/formtables/TBL_smoker.sql
+++
@@ -1,8 +0,0 @@
-create table IF NOT EXISTS smoker(value_id smallint not null primary key, property varchar(20) not null);
-
---Inserts
-
-insert into smoker values(0, 'Do You Smoke:');
-insert into smoker values(1, 'Yes');
-insert into smoker values(2, 'No');
-insert into smoker values(4, 'Casual');
