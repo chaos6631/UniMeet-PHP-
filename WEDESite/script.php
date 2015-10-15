3913a0fa58ac7093093da42381e18482157d7603
chaos6631 <chriscalder@rogers.com>
2015-10-15 16:19:59 -0400

    remove script.php

--- WEDESite/script.php
+++
@@ -1,5 +0,0 @@
-$query1 = "CREATE TABLE users(user_name CHAR(25) NOT NULL PRIMARY KEY, user_pass CHAR(32) NOT NULL)";
-$query2 = "INSERT INTO users(user_name, user_pass) VALUES('homer12', '5f4dcc3b5aa765d61d8327deb882cf99')";
-$query3 = "INSERT INTO users(user_name, user_pass) VALUES('flanders99', '5f4dcc3b5aa765d61d8327deb882cf99')";
-$query4 = "INSERT INTO users(user_name, user_pass) VALUES('maggie04', '5f4dcc3b5aa765d61d8327deb882cf99')";
-$query5 = "INSERT INTO users(user_name, user_pass) VALUES('vanhouten32', '5f4dcc3b5aa765d61d8327deb882cf99')";
